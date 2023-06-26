6068bd2f92e3cc00e824c3fb59c4605fa8cb5645
ChandraWiKu <ravieldark27@gmail.com>
2023-06-26 21:39:36 +0700

    add small changes

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,15 @@ const App = () => {
   return (
     <>
       {
-        isMobile ? /*!warning && */(
+        isMobile && !warning ? (
           <section className='fixed w-full h-[100vh] z-50'>
-            <div /*onClick={() => setWarning(true)}*/ className='flex justify-center items-center bg-gray-900 bg-opacity-95 w-full h-full'>
+            <div onClick={() => setWarning(true)} className='flex justify-center items-center bg-gray-900 bg-opacity-95 w-full h-full'>
               <div className='flex flex-col justify-start items-center bg-tertiary w-8/12 p-10 gap-5'>
                 <h1 className=' text-4xl text-yellow-400'>Warning!</h1>
                 <p className='text-center'>
                   because the size of webiste user who using mobile device i recomended you for using laptop or higher for better experience
                 </p>
-                {/* <button className='text-blue-700'>Click Anyware</button> */}
+                <button className='text-blue-700'>Click Anyware</button>
               </div>
             </div>
           </section>
